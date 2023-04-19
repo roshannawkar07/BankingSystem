566849d217a0e539c897f2cb71184a1330f9d74f
roshannawkar07 <131225877+roshannawkar07@users.noreply.github.com>
2023-04-19 08:59:19 +0530

    Delete The-Sparks-Foundation-main directory

--- The-Sparks-Foundation-main/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('localhost','root','','Roshan');
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
